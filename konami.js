5dfbfb5b6cbea9718b80206623f05dc99c10e301
Rory Hayes <roryohayes@gmail.com>
2016-04-21 17:07:25 -0700

    update header

--- konami.js
+++ konami.js
@@ -1,13 +1,7 @@
 /*
- * Konami-JS ~ 
- * :: Now with support for touch events and multiple instances for 
- * :: those situations that call for multiple easter eggs!
- * Code: http://konami-js.googlecode.com/
- * Examples: http://www.snaptortoise.com/konami-js
- * Copyright (c) 2009 George Mandis (georgemandis.com, snaptortoise.com)
- * Version: 1.4.5 (3/2/2016)
+ * Hidden-Konami-JS
+ * Code: https://github.com/rorosaurus/hidden-konami-js
  * Licensed under the MIT License (http://opensource.org/licenses/MIT)
- * Tested in: Safari 4+, Google Chrome 4+, Firefox 3+, IE7+, Mobile Safari 2.2.1 and Dolphin Browser
  */
 
 var functionHash = "";
